58eab9f986f96c6d9a5624324ced383e8647c0e6
Kichuk Rostyslav <ros.kichuk@gmail.com>
2023-05-18 13:32:15 +0300

    add test component 18.05 -ros

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
 import logo from './logo.svg';
 import './App.css';
-
+import Test from './components/Test';
 function App() {
   return (
     <div className="App">
-
+      <Test/>
     </div>
   );
 }
