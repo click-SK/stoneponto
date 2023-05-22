eb47380e0ed46d42437a52604bff1baff5504288
click-SK <sashakushnir4991@gmail.com>
2023-05-22 20:37:21 +0300

    add style main page 22/05 Alex

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,11 +1,28 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import '../style/header.scss'
 const Header = () => {
   return (
-    <div>
-      <Link to="/">Головна</Link>
-      <Link to="/admin">Адмін панель</Link>
-      <Link to="/calculator">Калькулятор</Link>
+    <div className="header_wraper">
+      <div className="contact_login_block">
+        <div className="wraper">
+          <span className="wrap_item text">+380 50 976 7417</span>
+          <span className="wrap_item text">+380 96 626 7481</span>
+          <span className="wrap_item text">ponto-print@ukr.net</span>
+          <span className="wrap_item"><button className="button_login">Log In</button></span>
+          
+        </div>
+      </div>
+      <div className="nav_logo_wraper">
+        <div className="logo">
+            <img src="./img/logo.png" alt="" />
+        </div>
+        <div className="navbar">
+        <Link to="/"><p className="link_route">Головна</p></Link>
+        <Link to="/admin"> <p className="link_route" >Адмін панель</p></Link>
+        <Link to="/calculator"><p className="link_route" >Калькулятор</p></Link>
+        </div>
+      </div>
     </div>
   );
 };
