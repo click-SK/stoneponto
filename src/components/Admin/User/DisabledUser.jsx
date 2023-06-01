60052d818387a4fb94e45d344c12f1d083ec2789
Kichuk Rostyslav <ros.kichuk@gmail.com>
2023-06-01 10:47:38 +0300

    add blog component and several changes 01.05 -ros

---
+++ src/components/Admin/User/DisabledUser.jsx
@@ -0,0 +1,41 @@
+import React, {useState, useEffect} from 'react';
+const DisabledUser =  ({user, editPath, setIsFetch, title}) => {
+    const [isEditValue, setIsEditValue] = useState(false);
+    const [statusDisabled,setStatusDisabled] = useState(false);
+
+    useEffect(() => {
+        setStatusDisabled(user.disabled)
+    },[user])
+
+    const handleEditButtonSave = () => {
+    
+        fetch(editPath, {
+          method: 'PATCH',
+          headers: {
+            'Content-Type': 'application/json'
+          },
+          body: JSON.stringify({
+            userId: user._id,
+            value: !statusDisabled
+          })
+        })
+          .then((res) => res.json())
+          setTimeout(() => {
+            // window.location.reload();
+            setIsFetch(state => !state)
+          },1000)
+      };
+
+      
+    return (
+        <div>
+            <p>{title}</p>
+            <input
+            type='checkbox'
+            checked={statusDisabled}
+            onChange={handleEditButtonSave}/>
+        </div>
+    );
+};
+
+export default DisabledUser;
